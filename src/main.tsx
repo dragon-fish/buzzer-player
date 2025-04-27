81ad484baaa0b97d5b833471bca1237822a47a81
dragon-fish <dragon-fish@qq.com>
2025-04-28 00:56:32 +0800

    chore: more musics

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,24 @@ import { BuzzerPlayer } from './BuzzerPlayer'
 
 const $root = document.getElementById('root') as HTMLElement
 const baseURL = new URL(import.meta.env.BASE_URL, window.location.href)
+const musicList = [
+  {
+    label: 'Twinkle, Twinkle, Little Star',
+    url: './demos/Twinkle, Twinkle, Little Star.bzs',
+  },
+  {
+    label: 'Happy Birthday',
+    url: './demos/Happy Birthday.bzs',
+  },
+  {
+    label: 'Beethoven – Symphony No. 5 in C minor, Op. 67',
+    url: './demos/Beethoven – Symphony No. 5 in C minor, Op. 67.bzs',
+  },
+  {
+    label: 'Pachelbel – Canon in D',
+    url: './demos/Pachelbel – Canon in D.bzs',
+  },
+]
 
 const buzzer = new BuzzerPlayer({
   tempo: 120,
@@ -10,26 +28,68 @@ const buzzer = new BuzzerPlayer({
   volume: 0.5,
 })
 
+const MusicSelector = ({
+  list,
+  onChange,
+  ...props
+}: {
+  list: { label: string; url: string }[]
+  onChange: (url: string) => void
+}) => {
+  return (
+    <select
+      onChange={(e) => {
+        const selected = list.find(
+          (item) => item.label === (e.target as HTMLSelectElement).value
+        )
+        if (selected) {
+          onChange(selected.url)
+        }
+      }}
+      {...props}
+    >
+      {list.map((item) => (
+        <option key={item.label} value={item.label}>
+          {item.label}
+        </option>
+      ))}
+    </select>
+  )
+}
+
 const App = () => {
   const $notesInput = (<textarea id="bzs"></textarea>) as HTMLInputElement
+  const $musicSelector = (
+    <MusicSelector
+      style={{ width: '100%' }}
+      list={musicList}
+      onChange={(url) => {
+        loadScript(url)
+      }}
+    />
+  ) as HTMLSelectElement
 
-  fetch(new URL('./demos/Pachelbel – Canon in D.bzs', baseURL))
-    .then((res) => {
-      return res.text()
-    })
-    .then((text) => {
-      $notesInput.value = text
-    })
-    .catch((err) => {
-      console.error('Failed to fetch default tones:', err)
-    })
+  const loadScript = async (url: string) => {
+    const response = await fetch(new URL(url, baseURL).href)
+    if (!response.ok) {
+      console.error('Failed to load script:', response.statusText)
+      return
+    }
+    const text = await response.text()
+    $notesInput.value = text
+    buzzer.stop()
+  }
+
+  $musicSelector.value = musicList[0].label
+  loadScript(musicList[0].url)
 
   return (
     <main>
       <h1>Buzzer Player</h1>
       <p>Lightweight WebAudio library for browser‑side “buzzer” melodies.</p>
-      <div>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
         <label htmlFor="bzs">Buzzer Script (.BZS)</label>
+        {$musicSelector}
         {$notesInput}
       </div>
       <div
